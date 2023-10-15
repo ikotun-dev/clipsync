1680a521c2ec6ee1f12d45b0eed316f246edd76d
ikotun-dev <danlogan2003@gmail.com>
2023-10-15 23:55:44 +0100

    initial

---
+++ src/components/HeaderTop.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="h-screen lg:h-screen bg-black flex items-center justify-between">
+        <div class="w-100 ml-20">
+            <img  :src="require('../assets/pana.png')">
+        </div>
+
+        <div class="mr-64 mb-40 flex flex-col items-center justify-center ">
+            <h1 class="text-montserrat font-extrabold  text-blue-500 text-6xl mb-24">Clipsync</h1>
+            <h2 class="text-white font-montserrat font-bold mt-4">Enter your session code</h2>
+            
+               
+                <input class="mt-4 w-72 p-2 rounded-md focus:outline-none font-pop focus:ring-4 focus:ring-blue-500" type="text"/>
+
+                <button class="mt-4 w-72 h-10 bg-blue-500 rounded-md font-montserrat font-extrabold text-center text-white text-md hover:bg-blue-800">Create/Join Session</button>
+            
+           
+        </div>
+       
+    </div>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,55 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./src/**/*.vue'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '17' : '4.43rem',
+        '25' : '8.22rem',
+        '98' : '23.3rem',
+        '99' : '26rem',
+        '100' : '56rem'
+
+      },
+
+      fontFamily :{
+		'pop' : ['Poppins', 'sans-serif'],
+    'philosopher' : ['Philosopher', 'sans-serif'],
+    montserrat: ['Montserrat', 'sans-serif'],
+	},
+  fontWeight: {
+    bold: '600', // Add the bold variant
+  },
+  height:{
+    '100' : '35rem',
+    '11' : '2.75rem',
+    '27' : '8rem',
+    '120' : '80rem',
+    '200' : '180rem',
+    '300' : '240rem',
+  },
+  width:{
+    '100' : '41rem',
+    '102' : '45rem',
+    '120' : '75rem',
+    '115' : '60rem',
+    '86' : '21rem',
+    '11' : '2.7rem',
+    '70' : '21.5rem',
+    '97' : '25.3rem',
+    '110' : '73rem'
+  }, 
+  screens: {
+    // 'sm': '300px',  // Target iPhone 5 screen size and smaller
+    // 'md': '375px',  // Target iPhone 6/7/8 screen size and smaller
+    // 'lg': '500px',  // Target iPhone 6/7/8 Plus screen size and smaller
+    'sm': '640px',  // Small screens (mobile)
+    'md': '768px',  // Medium screens (tablet)
+    'lg': '1024px', // Large screens (desktop)
+    'xl': '1280px', // Extra-large screens (larger desktops)
+  },
+
+	},
   },
   plugins: [],
 }
-
