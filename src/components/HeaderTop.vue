48bc42adc3b8c98ab8d0afdc36a6282729589be2
ikotun-dev <danlogan2003@gmail.com>
2023-10-20 20:25:48 +0100

    trying responsiveness

--- src/components/HeaderTop.vue
+++ src/components/HeaderTop.vue
@@ -1,15 +1,16 @@
 <template>
-    <div class="h-screen lg:h-screen bg-black flex items-center justify-between">
-        <div class="w-100 ml-20">
+    <div class="h-screen lg:h-screen bg-black flex items-center justify-center lg:justify-between">
+        <div class="w-100 ml-20 hidden lg:block">
             <img  :src="require('../assets/pana.png')">
         </div>
 
-        <div class="mr-64 mb-40 flex flex-col items-center justify-center ">
-            <h1 class="text-montserrat font-extrabold  text-blue-500 text-6xl mb-24">Clipsync</h1>
+        <div class="lg:mr-64 md:mr-52 mb-40 flex flex-col items-center justify-center ">
+            <!-- <img class="w-20" :src="require('../assets/logoclip2.png')"> -->
+            <h2 class="text-blue-500 text-2xl font-montserrat font-bold mt-4">Digipulse</h2>
             <h2 class="text-white font-montserrat font-bold mt-4">Enter your session code</h2>
             
                
-                <input class="mt-4 w-72 p-2 rounded-md focus:outline-none font-pop focus:ring-4 focus:ring-blue-500" type="text"/>
+                <input class="mt-4 w-72 p-2 rounded-md focus:outline-none font-semibold font-pop focus:ring-4 focus:ring-blue-500" type="text"/>
 
                 <button class="mt-4 w-72 h-10 bg-blue-500 rounded-md font-montserrat font-extrabold text-center text-white text-md hover:bg-blue-800">Create/Join Session</button>
             
@@ -18,3 +19,12 @@
        
     </div>
 </template>
+<script>
+export default{
+    data(){ 
+        return {
+            sessionToken : null,
+        }
+    }
+}
+</script>
--- src/components/MajorPage.vue
+++ src/components/MajorPage.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="lg:h-screen bg-black">
         <div class="">
-            <h2 class="ml-7 pt-10 pb-3 text-blue-500 font-montserrat text-lg font-extrabold">Clipsync</h2>
+            <h2 class="ml-7 pt-10 pb-3 text-blue-500 font-montserrat text-lg font-extrabold">Digipulse</h2>
         </div>
         <div class="flex flex-col">
             <h4 class="ml-12 m-1 text-sm text-white font-montserrat font-bold">Type here</h4>
