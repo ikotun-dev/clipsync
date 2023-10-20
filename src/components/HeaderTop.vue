66bda4b30dbcbee2e0376678d3a34932c92090d9
ikotun-dev <danlogan2003@gmail.com>
2023-10-20 21:32:16 +0100

    some config

--- src/components/HeaderTop.vue
+++ src/components/HeaderTop.vue
@@ -10,35 +10,40 @@
             <h2 class="text-white font-montserrat font-bold mt-4">Enter your session code</h2>
             
                
-                <input class="mt-4 w-72 p-2 rounded-md focus:outline-none font-semibold font-pop focus:ring-4 focus:ring-blue-500" v-model="sessionToken" type="text"/>
+                <input class="mt-4 w-72 p-2 rounded-md focus:outline-none font-semibold font-pop focus:ring-4 focus:ring-blue-500"  type="text"/>
 
-                <button class="mt-4 w-72 h-10 bg-blue-500 rounded-md font-montserrat font-extrabold text-center text-white text-md hover:bg-blue-800">Create/Join Session</button>
-            
+                <button v-if="processing == false" class="mt-4 w-72 h-10 bg-blue-500 rounded-md font-montserrat font-extrabold text-center text-white text-md hover:bg-blue-800">Create/Join Session</button>
+                <button v-else class="h-10 w-72 mt-4 py-1 bg-blue-500 flex items-center justify-center">
+                    <div class=" animate-spin rounded-full border-l-4 border-b-4 border-t-4 w-8 h-8 border-white border-opacity-100 "></div>
+                </button>
+              
            
         </div>
        
     </div>
 </template>
 <script>
-import VueNativeSock from 'vue-native-websocket';
+
+//import VueNativeSock from 'vue-native-websocket';
 
 export default{
     data(){ 
         return {
+            processing : true,
             sessionToken : null,
         }
     },
     methods : { 
        
     },
-    created() {
-    this.$options.sockets = {
-      mySocket: new VueNativeSock({
-        debug: true,
-        connection: 'ws://127.0.0.1:8000', // Replace with your actual WebSocket backend URL
-      }),
-    };
-}
+//     created() {
+//     this.$options.sockets = {
+//       mySocket: new VueNativeSock({
+//         debug: true,
+//         connection: 'ws://127.0.0.1:8000', // Replace with your actual WebSocket backend URL
+//       }),
+//     };
+// }
 
 }
 </script>
