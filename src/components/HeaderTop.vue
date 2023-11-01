001448f6f19e66a4e5ed7d7e50dade3453052c39
ikotun-dev <danlogan2003@gmail.com>
2023-11-01 10:47:00 +0100

    changed label

--- src/components/HeaderTop.vue
+++ src/components/HeaderTop.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="h-screen bg-gray-900 flex items-center justify-center lg:justify-between">
-        <div class="w-100 ml-20 hidden lg:block">
+        <div class="w-100 ml-20 hidden md:mr-14 md:block">
             <img  :src="require('../assets/pana.png')">
         </div>
 
-        <div class="lg:mr-64  mb-40 flex flex-col items-center justify-center ">
+        <div class="lg:mr-64  mb-40 flex flex-col items-center justify-center md:mr-6">
             <!-- <img class="w-20" :src="require('../assets/logoclip2.png')"> -->
             <h2 class="text-blue-500 text-2xl font-montserrat font-bold mt-4">Drop.it</h2>
             <h2 class="text-white font-montserrat font-bold mt-4">Enter your session code</h2>
@@ -12,7 +12,7 @@
                
                 <input class="mt-4 w-72 p-2 rounded-md focus:outline-none font-semibold font-pop focus:ring-4 focus:ring-blue-500"  type="text"/>
 
-                <button v-if="processing == false" class="mt-4 w-72 h-10 bg-blue-500 rounded-md font-montserrat font-extrabold text-center text-white text-md hover:bg-blue-800">Create/Join Session</button>
+                <button v-if="processing == false" class="mt-4 w-72 h-10 bg-blue-500 rounded-md font-montserrat font-extrabold text-center text-white text-md hover:bg-blue-800">Initiate Session</button>
                 <button v-else class="h-10 rounded-md w-72 mt-4 py-1 bg-blue-500 flex items-center justify-center">
                     <div class=" animate-spin rounded-full border-l-4 border-b-4 border-t-4 w-8 h-8 border-white border-opacity-100 "></div>
                 </button>
--- src/components/MajorPage.vue
+++ src/components/MajorPage.vue
@@ -6,13 +6,22 @@
         <div class="flex flex-col">
             <h4 class="ml-12 m-1 text-sm text-white font-montserrat font-bold">Type here</h4>
 
-            <input class="text-gray-200 mt-2 mx-6 lg:ml-20 lg:mr-20 bg-gray-600 rounded-md lg:h-16 h-14 focus:outline-none p-3 font-pop" type="text"/>
+            <input
+                class="text-gray-200 mt-2 mx-6 lg:ml-20 lg:mr-20 bg-gray-600 rounded-md lg:h-16 text-xs h-14 focus:outline-none p-3 font-pop"
+                type="text" />
 
-            <button class="absolute right-2 mt-32 font-extrabold font-montserrat  text-white mr-6 lg:mr-20 bg-blue-500 w-20 h-8 rounded-md text-xs hover:bg-blue-800">send</button>
+            <!-- <div class="flex items-center justify-end mt-9 mr-48 ">
+                <label for="fileInput" class="cursor-pointer mr-2 text-blue-500 font-pop-bold font-bold  w-30 px-4 hover:text-gray-300"> + attach file
+                    <input id="fileInput" class="hidden" name="attach file" type="file" />
+                </label>
+               
+            </div> -->
+
+            <button
+                class="absolute right-2 mt-32 font-extrabold font-montserrat  text-white mr-6 lg:mr-20 bg-blue-500 w-20 h-8 rounded-md text-xs hover:bg-blue-800">send</button>
         </div>
         <div class="ml-12 mb-24 w-32 h-8 rounded-sm bg-gray-600 absolute bottom-0 left-0 flex items-center justify-center">
             <i class="ml-2 mb-2 text-4xl text-green-500 mr-1">&#8226;</i>
             <h4 class="font-montserrat font-extrabold text-gray-300 text-xs">3 connected</h4>
         </div>
-    </div>
-</template>
+</div></template>
