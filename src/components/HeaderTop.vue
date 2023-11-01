d4a1490a949c23e8deb78a4fd0d186c7e4672056
ikotun-dev <danlogan2003@gmail.com>
2023-11-01 11:21:03 +0100

    vals

--- src/components/HeaderTop.vue
+++ src/components/HeaderTop.vue
@@ -12,7 +12,7 @@
                
                 <input class="mt-4 w-72 p-2 rounded-md focus:outline-none font-semibold font-pop focus:ring-4 focus:ring-blue-500"  type="text"/>
                 <h4 v-show="no_value == true" class="text-red-600 font-montserrat mt-2 font-extrabold">Invalid session code </h4>
-                <button v-if="processing == false" class="mt-4 w-72 h-10 bg-blue-800 rounded-md font-montserrat font-extrabold text-center text-white text-md hover:bg-blue-500" @click="checkSession(), validateSession()">Initiate Session</button>
+                <button v-if="processing == false" class="mt-2 w-72 h-10 bg-blue-800 rounded-md font-montserrat font-extrabold text-center text-white text-md hover:bg-blue-500" @click="checkSession(), validateSession()">Initiate Session</button>
                 <button v-else class="h-10 rounded-md w-72 mt-4 py-1 bg-blue-500 flex items-center justify-center">
                     <div class=" animate-spin rounded-full border-l-4 border-b-4 border-t-4 w-8 h-8 border-white border-opacity-100 "></div>
                 </button>
