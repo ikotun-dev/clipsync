9557270432c83f4c2e84193dc01e5b3eee38edc0
ikotun-dev <danlogan2003@gmail.com>
2023-11-01 17:06:59 +0100

    copyright

--- src/components/FooterButtom.vue
+++ src/components/FooterButtom.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-screen flex bg-gray-800">
-        <h3>collins</h3>
+    <div class="w-screen flex justify-center lg:h-20 h-20 bg-blue-800">
+        <h3 class="mt-4 font-semibold font-montserrat text-white">Collins&copy; 2023</h3>
     </div>
 </template>
